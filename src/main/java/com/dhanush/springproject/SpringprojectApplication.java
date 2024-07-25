be35385eaf831383dbd64b101ae46ea4f4cc0f53
dhanushgr <grdhanush8@gmail.com>
2024-07-26 03:50:31 +0530

    simple modification  (not an imp commit)

--- src/main/java/com/dhanush/springproject/SpringprojectApplication.java
+++ src/main/java/com/dhanush/springproject/SpringprojectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springproject1Application {
+public class SpringprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springproject1Application.class, args);
+		SpringApplication.run(SpringprojectApplication.class, args);
 	}
 
 }
--- src/main/java/com/dhanush/springproject/controller/loadController.java
+++ src/main/java/com/dhanush/springproject/controller/loadController.java
@@ -1,14 +1,8 @@
 package com.dhanush.springproject.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class loadController {
-
-  @GetMapping("/home")
-  public String home() {
-    return "home";
-  }
 
 }
